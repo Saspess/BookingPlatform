a8129d19eeb569c6dc3218a97aeb351c22bf8683
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2024-07-04 20:06:10 +0300

    Add IdentityMS data layer

---
+++ src/Microservices/IdentityMS/BP.IdentityMS.Data/Constants/SectionNames.cs
@@ -0,0 +1,7 @@
+﻿namespace BP.IdentityMS.Data.Constants
+{
+    internal static class SectionNames
+    {
+        public const string MongoDb = "MongoDb";
+    }
+}
---
+++ src/Microservices/IdentityMS/BP.IdentityMS.Data/Repositories/Contracts/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using BP.IdentityMS.Data.Entities;
+
+namespace BP.IdentityMS.Data.Repositories.Contracts
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<UserEntity>> GetAllAsync();
+        Task<UserEntity> GetByIdAsync(string id);
+        Task CreateAsync(UserEntity entity);
+        Task UpdateAsync(UserEntity entity);
+        Task DeleteAsync(string id);
+    }
+}
