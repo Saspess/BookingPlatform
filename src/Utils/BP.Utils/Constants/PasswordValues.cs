cc8f982dc26332ecbd483cddd49d9cd11f7ad47a
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2024-07-07 19:12:39 +0300

    Add password hashing utils

---
+++ src/Utils/BP.Utils/Constants/PasswordValues.cs
@@ -0,0 +1,9 @@
+﻿namespace BP.Utils.Constants
+{
+    internal static class PasswordValues
+    {
+        public const int SaltBytes = 16;
+        public const string Separator = ":";
+        public const int SaltPosition = 1;
+    }
+}
